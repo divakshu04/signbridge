05c6595d6dd48a5a0dae803ae29e1f95d796144f
divakshu04 <divakshusharma@gmail.com>
2026-04-29 00:21:02 +0530

    backend-url

--- signbridge-server/main.py
+++ signbridge-server/main.py
@@ -1,7 +1,7 @@
+import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-import os
 import json
 import numpy as np
 from collections import deque, Counter
